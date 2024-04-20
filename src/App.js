f01978f54dd48efa5d3118c7508fc2124c42a75f
sqVish99 <vmharish@asu.edu>
2024-04-20 01:08:24 -0700

    First Commit - New Portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from './sections/Home/Home';
+import About from './sections/About/About';
+import Projects from './sections/Projects/Projects';
+import Contact from './sections/Contact/Contact';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section id="home" className="home-section"><Home /></section>
+      <section id="about" className="about-section"><About /></section>
+      <section id="projects" className="projects-section"><Projects /></section>
+      <section id="contact" className="contact-section"><Contact /></section>
     </div>
   );
 }
---
+++ src/sections/Projects/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Projects.css';
+
+const Projects = () => {
+    return (
+        <div className="projects-container">
+            <h1>Projects!</h1>
+        </div>
+    );
+}
+
+export default Projects;
